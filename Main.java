a8e5ce92bdafe99afc05cd8c3e1bfe395e1117bf
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-05-05 08:35:44 +0200

    1 zadanie

--- Main.java
+++ Main.java
@@ -1,2 +1,28 @@
+import company.models.Worker;
+import company.models.Manager;
+
 public class Main {
+    public static void main(String[] args) {
+        Worker worker1 = new Worker("John Doe", 2500, 1);
+        Worker worker2 = new Worker("Kristen James", 3000, 2);
+        Worker worker3 = new Worker("Oliver Power", 2200, 3);
+        Worker worker4 = new Worker("Raphael Jacobs", 2800, 4);
+
+        Manager manager = new Manager("Edward Rain", 5000, 5);
+
+        System.out.println("Salary of worker 1: " + worker1.getName() + ": " + worker1.getSalary());
+        worker1.work();
+
+        System.out.println("Salary of worker 2: " + worker2.getName() + ": " + worker2.getSalary());
+        worker2.work();
+
+        System.out.println("Salary of worker 3: " + worker3.getName() + ": " + worker3.getSalary());
+        worker3.work();
+
+        System.out.println("Salary of worker 4: " + worker4.getName() + ": " + worker4.getSalary());
+        worker4.work();
+
+        System.out.println("Salary of manager: " + manager.getName() + ": " + manager.getSalary());
+        manager.work();
+    }
 }
---
+++ company/interfaces/Employable.java
@@ -0,0 +1,4 @@
+package company.interfaces;
+public interface Employable {
+    void work();
+}
